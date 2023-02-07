22fd3ba5a212bfba8b731633880ad5e0f58a9508
Stephen Jones <stephen-jones@hotmail.co.uk>
2023-02-07 11:52:01 +0000

    body in substring added to articleCard

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -6,7 +6,7 @@ function ArticleCards({article}) {
                 <img src={article.article_img_url} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{article.title}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">{article.body.substring(0,100)}...</p>
                     <a  className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
